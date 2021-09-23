95468611b6432ff55adcced38775d11e925afcb2
rgoodman22 <goodman.rob24@gmail.com>
2021-09-23 16:40:03 -0500

    submitted conditionals and loops

--- solutions.js
+++ solutions.js
@@ -1,1 +1,15 @@
-export const sumOfSquares = (x, y) => x + y;
+//conditionals
+export const sumOfSquares = (x, y) => x * x + y * y;
+
+export const maxOfThree = (x, y, z) => Math.max(x, y, z);
+
+export const isLeapYear = (y) => y % 4 === 0 && (y % 100 !== 0 || y % 400 === 0);
+
+//loops
+export const allEvens = a => a.filter(e => e % 2 === 0);
+
+export const listSquares = a => a.map(e => e * e);
+
+export const listSum = a => a.reduce((prev, cur) => prev + cur, 0);
+
+export const listSquaresSum = a => a.reduce((prev, cur) => prev + cur * cur, 0);
